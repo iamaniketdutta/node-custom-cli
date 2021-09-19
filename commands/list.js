a4eb6ba5c39d6629ea296f4c0d85cf03e3bf5329
iamaniketdutta <iamaniketdutta@gmail.com>
2021-09-20 04:51:34 +0530

    Fix an logical issue causing display isssue in cmd
    This change fixed the while adding display to the console as _id and __v fields are meant to abstract from user's view.

--- commands/list.js
+++ commands/list.js
@@ -1,18 +1,25 @@
-const Notes = require('../dbHandlers/notesDb');
+const {Notes} = require('../dbHandlers/notesDb');
 const chalk = require('chalk');
 const codes = require('../conf/codes.json');
 const logger = require('../logger');
 
+
+/**
+ * 
+ * @param {object} args Arguments type with value for list sub-command 
+ */
 async function list (args) {
     try {
+        // curating find condition object with name like and status
         const findCondition = {name : { $regex: args.name, $options: 'i' }, status : codes.ENABLED};
         const notesRecords = await Notes.find(findCondition).lean();
-        //display message to user
+        // display message to user
         if(typeof notesRecords !== 'undefined' && notesRecords.length) {
+            // enumerate notes collection to curate the console data with all fields except _id
             notesRecords.forEach(element => {
                 console.log('----------------------------');
                 for (const [key, value] of Object.entries(element)) {
-                    if (key !== '_id')
+                    if (!['_id', '__v'].includes(key)) 
                     console.log(chalk.greenBright(`${key.toUpperCase()}: ${value}`));
                 }
                 console.log('----------------------------');
--- commands/select.js
+++ commands/select.js
@@ -1,21 +1,27 @@
-const Notes = require('../dbHandlers/notesDb');
+const {Notes} = require('../dbHandlers/notesDb');
 const chalk = require('chalk');
 const codes = require('../conf/codes.json');
 const logger = require('../logger');
 
 
+/**
+ * 
+ * @param {object} args Arguments type with value for select sub-command
+ */
 async function select (args) {
     try {
+        // curating find condition object with name like and status
         const findCondition = {name : { $regex: args.name, $options: 'i' }, status : codes.ENABLED};
         let selectCondition = {};
         if (args.field && typeof args.field === 'object') {
+            // enumerate field argument to select only the specific field
             args.field.forEach(element => selectCondition[element] = 1);
         }
         const notesRecord = await Notes.findOne(findCondition).select(selectCondition).lean();
         //display message to user
         if(typeof notesRecord !== 'undefined' && notesRecord) {
             for (const [key, value] of Object.entries(notesRecord)) {
-                if (key !== '_id')
+                if (!['_id', '__v'].includes(key))
                 console.log(chalk.greenBright(`${key.toUpperCase()}: ${value}`));
             }
         } else {
